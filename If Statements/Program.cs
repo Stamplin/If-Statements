81909a08bb5ff8accf1e59e1791392dc42ccce22
Stamplin <701017313@gotvdsb.ca>
2024-09-17 09:33:08 -0400

    Program.cs

--- If Statements/Program.cs
+++ If Statements/Program.cs
@@ -1,9 +1,9 @@
-﻿//SPACE BOXING
+//SPACE BOXING
 
-double weight, selection, venus, mars, jupiter, saturn, uranus, neptune; 
+double weight, selection, venus, mars, jupiter, saturn, uranus, neptune;
 venus = 0.78;
 mars = 0.39;
-jupiter = 2.65; 
+jupiter = 2.65;
 saturn = 1.17;
 uranus = 1.05;
 neptune = 1.23;
@@ -11,7 +11,7 @@ Console.WriteLine("\r\n #1 Venus \r\n #2 Mars \r\n #3 Jupiter \r\n #4 Saturn \r\
 Console.WriteLine("Please enter your current weight on earth: "); //ASK FOR WEIGHT FOR THE MATH LATER ON
 double.TryParse(Console.ReadLine(), out weight);
 Console.WriteLine("Select a planet listed above: "); //SELECTION TO SEE WHICH TO PICK FOR THE MATH
-double.TryParse(Console.ReadLine(),out selection);
+double.TryParse(Console.ReadLine(), out selection);
 if (selection == 1)
 {
     Console.WriteLine("Your weight would be: " + Math.Round(weight * venus));
@@ -42,20 +42,57 @@ Console.WriteLine("Press enter to move to the next part");
 Console.Clear();
 
 //SILLY GUESSING GAME
-int spesNum;
+int spesNum, numGuess;
 spesNum = 7;
 Console.WriteLine("Welcoming to the goofy ahh guessing game please input a number from 1-10");
 Console.WriteLine("Your number:");
-int.TryParse(Console.ReadLine(), out spesNum);
-if (spesNum == 7)
+int.TryParse(Console.ReadLine(), out numGuess);
+if (numGuess == spesNum)
 {
-    Console.WriteLine("Bingo correct it is 7!!");
+    Console.WriteLine("Bingo correct!!");
 }
-else if (spesNum < 10)
+else if (numGuess != spesNum)
 {
-    Console.WriteLine("Your number is lower than 10!");
+    Console.WriteLine("Wrong number! The number is " + spesNum);
 }
-else if (spesNum > 10)
+
+
+//Mini Wuiz
+int quizScore, quizQ1;
+quizScore = 0;
+Console.WriteLine("Welcome to the mini quiz, where you'll be asked mini sized questions.");
+Console.WriteLine("");
+Console.WriteLine("Question 1: What is 8^2+5?");
+//prep for the mini quiz
+int.TryParse(Console.ReadLine(), out quizQ1);
+//question 1 (Numbers)
+if (quizQ1 == 69)
 {
-    Console.WriteLine("Your number is higher than 10!");
+    Console.WriteLine("Correct. Nice. +1p"); quizScore = +1;
 }
+else if (quizQ1 <= 70 && quizQ1 >= 60)
+{
+    Console.WriteLine("Very close but that aint right, Nice try.");
+}
+else if (quizQ1 > 70 || quizQ1 < 60)
+{
+    Console.WriteLine("Yeah.. Nah you're not even close.");
+}
+//question 2 (writing)
+string quizQ2;
+Console.WriteLine();
+Console.WriteLine("Are you dead? \n There only one answer don't lie.");
+quizQ2 = Console.ReadLine().ToUpper();
+if (quizQ2 == "YES")
+{
+    Console.WriteLine("Dead people dont do quizzes, you liar. No score for you.");
+}
+else if (quizQ2 == "NO")
+{
+    Console.WriteLine("Make sense... aight u get a point. +1p"); quizScore = +1;
+}
+//question 3 mutiple choice
+Console.WriteLine();
+
+
+Console.ReadLine();
