171618baf7f012da097561abb8bfaa54f131cfe1
Stamplin <701017313@gotvdsb.ca>
2024-09-18 09:07:25 -0400

    Update Program.cs

--- If Statements/Program.cs
+++ If Statements/Program.cs
@@ -1,5 +1,7 @@
 //SPACE BOXING
 
+using System.Runtime.InteropServices;
+
 double weight, selection, venus, mars, jupiter, saturn, uranus, neptune;
 venus = 0.78;
 mars = 0.39;
@@ -36,7 +38,6 @@ else if (selection == 6)
 {
     Console.WriteLine("Your weight would be: " + Math.Round(weight * neptune));
 }
-
 Console.ReadLine();
 Console.WriteLine("Press enter to move to the next part");
 Console.Clear();
@@ -61,7 +62,8 @@ Console.WriteLine("Press enter to move to the next part");
 Console.Clear();
 
 //Mini Quiz
-int quizScore, quizQ1, quizQ2;
+int quizScore, quizQ1;
+string quizQ2;
 quizScore = 0;
 Console.WriteLine("Welcome to the mini quiz, where you'll be asked mini sized questions.");
 Console.WriteLine("");
@@ -71,7 +73,8 @@ int.TryParse(Console.ReadLine(), out quizQ1);
 //question 1 (Numbers)
 if (quizQ1 == 69)
 {
-    Console.WriteLine("Correct. Nice. +1p"); quizScore = +1;
+    Console.WriteLine("Correct. Nice.");
+    quizScore = quizScore + 1;
 }
 else if (quizQ1 <= 70 && quizQ1 >= 60)
 {
@@ -91,11 +94,52 @@ if (quizQ2 == "YES")
 }
 else if (quizQ2 == "NO")
 {
-    Console.WriteLine("Make sense... aight u get a point. +1p"); quizScore = +1;
+    Console.WriteLine("Make sense... aight u get a point.  ");
+    quizScore = quizScore + 1;
 }
 //question 3 mutiple choice
+string quizQ3;
 Console.WriteLine();
 Console.WriteLine("Multiple choice now!");
 Console.WriteLine("");
+
+Console.WriteLine("What number was the Apollo mission that successfully put a man on the moon for the first time in human history? ");
+Console.WriteLine("A. Apollo 16");
+Console.WriteLine("B. Apollo 13");
+Console.WriteLine("C. Apollo 11");
+Console.WriteLine("D. Apollo 15");
+quizQ3 = Console.ReadLine().ToUpper();
+if (quizQ3 == "C")
+{
+    Console.WriteLine("Correct. Apollo 11 marked the first time in history that humans landed on the Moon.  "); 
+    quizScore = quizScore + 1;
+}
+else if (quizQ3 == "B")
+{
+    Console.WriteLine("Close. Apollo 13 is not the first mission that put a man on the moon.");
+}
+else if (quizQ3 == "A" || quizQ3 == "D")
+{
+    Console.WriteLine("Not even close.");
+}
+//True or False 
+string tofQuiz;
+Console.WriteLine("Last one!");
+Console.WriteLine();
+Console.WriteLine("True or False. Hawaiian pizza comes from Canada.");
+tofQuiz = Console.ReadLine().ToUpper();
+if (tofQuiz == "TRUE")
+{
+    Console.WriteLine("Correct. it was created in 1962 by Sam Panopoulos at the Satellite Restaurant in Ontario, Canada.");
+    quizScore = quizScore + 1;
+}
+else if (tofQuiz != "TRUE")
+{
+    Console.WriteLine("Nope, it's actually true.");
+}
+Console.WriteLine();
+Console.WriteLine("Time for the results!!!");
+Console.WriteLine("You got " + quizScore + " out of 4 questions right!");
+Console.WriteLine("That's " + ((quizScore/4)*100)  + "% !!!");
 
 Console.ReadLine();
